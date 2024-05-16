1e337aaab51dd4736345897b543f5ec0e31d0daa
NamfonSaphu <namfonsaphu@gmail.com>
2024-05-16 23:52:21 +0700

    edit index

--- dashboard.php
+++ dashboard.php
@@ -44,16 +44,13 @@
                 }
             ?>
 
-            <h1 class="display-5 fw-bold text-body-emphasis">Welcome <?php echo $userData['username'] ?></h1>
-            <div class="col-lg-6 mx-auto">
-                <p class="lead mb-4">Email: <?php echo $userData['email'] ?></p>
-            </div>
+            <h1 class="display-5 fw-bold text-body-emphasis">Welcome to Mydeshboard <?php echo $userData['username'] ?></h1>
+            
+
         </div>
         <!-- heroes end-->
 
-        <!-- footer -->
-        <?php include('footer.php');?>
-        <!-- footer end-->
+       
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
